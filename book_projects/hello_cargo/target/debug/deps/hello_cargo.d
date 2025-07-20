73c2df1b752f8f8a585300ea72330c1322d09191
zevirno <dobryninforwork@gmail.com>
2025-07-20 21:09:55 +0300

    build hello_cargo

---
+++ book_projects/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+src\main.rs:
---
+++ book_projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\hello_cargo\src\main.rs
