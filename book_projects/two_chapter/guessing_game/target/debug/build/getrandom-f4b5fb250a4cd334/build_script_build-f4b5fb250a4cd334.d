a79968ee3dc2ba7f47a7c784fd917b06e4e08012
zevirno <dobryninforwork@gmail.com>
2025-07-22 14:07:01 +0300

    add lib rand

---
+++ book_projects/two_chapter/guessing_game/target/debug/build/getrandom-f4b5fb250a4cd334/build_script_build-f4b5fb250a4cd334.d
@@ -0,0 +1,5 @@
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\two_chapter\guessing_game\target\debug\build\getrandom-f4b5fb250a4cd334\build_script_build-f4b5fb250a4cd334.d: C:\Users\Zevirno\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.3\build.rs
+
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\two_chapter\guessing_game\target\debug\build\getrandom-f4b5fb250a4cd334\build_script_build-f4b5fb250a4cd334.exe: C:\Users\Zevirno\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.3\build.rs
+
+C:\Users\Zevirno\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.3\build.rs:
---
+++ book_projects/two_chapter/guessing_game/target/debug/build/zerocopy-feb6fbffbb72158c/build_script_build-feb6fbffbb72158c.d
@@ -0,0 +1,5 @@
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\two_chapter\guessing_game\target\debug\build\zerocopy-feb6fbffbb72158c\build_script_build-feb6fbffbb72158c.d: C:\Users\Zevirno\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zerocopy-0.8.26\build.rs
+
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\two_chapter\guessing_game\target\debug\build\zerocopy-feb6fbffbb72158c\build_script_build-feb6fbffbb72158c.exe: C:\Users\Zevirno\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zerocopy-0.8.26\build.rs
+
+C:\Users\Zevirno\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zerocopy-0.8.26\build.rs:
