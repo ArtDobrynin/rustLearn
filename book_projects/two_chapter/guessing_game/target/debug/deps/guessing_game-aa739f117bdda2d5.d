6a9644e51cf374b3b76bab4369f71728185db1e2
zevirno <dobryninforwork@gmail.com>
2025-07-22 13:53:12 +0300

    Revert "Add func for read input line"
    This reverts commit f3a926021da6bb1de6d94e38c555bafd06cb436c.

--- book_projects/two_chapter/guessing_game/target/debug/deps/guessing_game-aa739f117bdda2d5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\two_chapter\guessing_game\target\debug\deps\guessing_game-aa739f117bdda2d5.d: src\main.rs
-
-C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\two_chapter\guessing_game\target\debug\deps\libguessing_game-aa739f117bdda2d5.rmeta: src\main.rs
-
-src\main.rs:
