57d4ca63a41d2d05c738cccc31049c26a77ccbb1
zevirno <dobryninforwork@gmail.com>
2025-07-22 23:22:37 +0300

    add loop

--- book_projects/two_chapter/guessing_game/src/main.rs
+++ book_projects/two_chapter/guessing_game/src/main.rs
@@ -8,20 +8,22 @@ fn main() {
     let secret_number = rand::rng().random_range(1..101);
     println!("Secret number: {}", secret_number);
 
-    println!("Please, get number: ");
+    loop{
+        println!("Please, get number: ");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Could not read the line!");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Could not read the line!");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Set number, please!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Set number, please!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Less secret number!"),
-        Ordering::Greater => println!("More secret number!"),
-        Ordering::Equal => println!("You win!")
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Less secret number!"),
+            Ordering::Greater => println!("More secret number!"),
+            Ordering::Equal => println!("You win!")
+        }
     }
 }
