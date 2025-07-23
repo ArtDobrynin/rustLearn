1af26703ead490deb0ef325e9f54189165dd7d35
zevirno <dobryninforwork@gmail.com>
2025-07-23 12:16:32 +0300

    error code for think about immut variables

--- book_projects/three_chapter/variables/src/main.rs
+++ book_projects/three_chapter/variables/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("Value x equal: {}", x);
+
+    x = 6;
+    println!("Value x equal: {}", x);
 }
---
+++ book_projects/three_chapter/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\three_chapter\variables\target\debug\deps\variables.d: src\main.rs
+
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\three_chapter\variables\target\debug\deps\variables.exe: src\main.rs
+
+src\main.rs:
