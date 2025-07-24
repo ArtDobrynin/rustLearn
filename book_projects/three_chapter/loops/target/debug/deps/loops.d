277e248c6a3e80aeefa6bd97ff16dc60c2e03ef3
zevirno <dobryninforwork@gmail.com>
2025-07-24 23:25:23 +0300

    add loop

---
+++ book_projects/three_chapter/loops/target/debug/deps/loops.d
@@ -0,0 +1,5 @@
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\three_chapter\loops\target\debug\deps\loops.d: src\main.rs
+
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\three_chapter\loops\target\debug\deps\loops.exe: src\main.rs
+
+src\main.rs:
