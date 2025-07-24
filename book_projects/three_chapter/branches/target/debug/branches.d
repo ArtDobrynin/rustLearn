e8666f673a0f6cf00c55ee2c70973959d32d03a9
zevirno <dobryninforwork@gmail.com>
2025-07-24 16:29:17 +0300

    add base condition for check number

---
+++ book_projects/three_chapter/branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\three_chapter\branches\target\debug\branches.exe: C:\Users\Zevirno\Desktop\Projects\rustLearn\book_projects\three_chapter\branches\src\main.rs
