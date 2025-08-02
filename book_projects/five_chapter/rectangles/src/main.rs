b3d3c171eba516707c8877d627571f4f65f5420a
artemDobrynin <adobrynin@tangl.cloud>
2025-08-02 22:12:22 +0300

    create programm for area rectangles

--- book_projects/five_chapter/rectangles/src/main.rs
+++ book_projects/five_chapter/rectangles/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let width = 70;
+    let height = 140;
+
+    println!("Area: {}", area(width, height));
+}
+
+fn area(w: i32, h:i32) -> i32 {
+    w * h
 }
