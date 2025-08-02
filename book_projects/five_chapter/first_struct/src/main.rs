c94a18e598e6b046fe4641be7f363fb9f8b86caf
artemDobrynin <adobrynin@tangl.cloud>
2025-08-02 21:55:55 +0300

    check error on type init

--- book_projects/five_chapter/first_struct/src/main.rs
+++ book_projects/five_chapter/first_struct/src/main.rs
@@ -1,6 +1,11 @@
-struct User<'a> {
+struct User {
     id: u32,
-    name: &'a str,
+    email: String,
+    hash_password: String
+}
+
+struct Player {
+    id: u32,
     email: String,
     hash_password: String
 }
@@ -10,9 +15,13 @@ fn main() {
         id: 666,
         email: String::from("bashmachok@gmail.com"),
         hash_password: String::from("@notfoundpassword123"),
-        name: "Toha"
-    }
+    };
 
-    println!("Name: {}", user_toha.name);
+    let player_toha = Player {
+        id: 777,
+        ..user_toha
+    };
+
+    println!("hash_password: {}", player_toha.hash_password);
     println!("Id: {}", user_toha.id);
 }
