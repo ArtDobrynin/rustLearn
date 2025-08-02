dc9aa6910990d7f26e96f83547322a25f27b0ff4
artemDobrynin <adobrynin@tangl.cloud>
2025-08-02 20:50:04 +0300

    add function first_word for fount first word

---
+++ book_projects/four_chapter/sliceis/sliceis/target/debug/deps/sliceis.d
@@ -0,0 +1,5 @@
+D:\rustLearn\book_projects\four_chapter\sliceis\sliceis\target\debug\deps\sliceis.d: src\main.rs
+
+D:\rustLearn\book_projects\four_chapter\sliceis\sliceis\target\debug\deps\sliceis.exe: src\main.rs
+
+src\main.rs:
