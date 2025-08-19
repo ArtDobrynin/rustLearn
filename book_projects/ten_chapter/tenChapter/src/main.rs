cc2742dbd60d6a20f3247d42bcd61a742bf082cc
bubbleFlash <donryninforworl@gmail.com>
2025-08-19 12:35:42 +0300

    add func find_max

--- book_projects/ten_chapter/tenChapter/src/main.rs
+++ book_projects/ten_chapter/tenChapter/src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut max = number_list[0];
+    let max = find_max(&number_list);
 
-    for number in number_list {
+    println!("Max number is {}", max);
+}
+
+fn find_max(list: &[i32]) -> i32 {
+    let mut max= list[0];
+
+    for &number in list.iter() {
         if number > max {
             max = number;
         }
     }
+
+    return max;
 }
